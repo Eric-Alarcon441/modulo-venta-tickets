8a935cc857b01d81c4298f7ea8d284c005a61937
Eric-Alarcon441 <ericalarcon441@gmail.com>
2021-10-20 22:56:35 -0500

    Base de datos finalizada con triggers

--- database/ventatickets.sql
+++ database/ventatickets.sql
@@ -2,50 +2,56 @@ CREATE Table role(
   id_role INT(11) PRIMARY KEY NOT NULL,
   descripcion VARCHAR(50) NOT NULL
 );
+CREATE Table usuario(
+  id_usuario INT(11) PRIMARY KEY NOT NULL,
+  id_role INT(11) NOT NULL,
+  email VARCHAR(30) NOT NULL,
+  password VARCHAR(30) NOT NULL,
+  FOREIGN KEY (id_role) REFERENCES role(id_role)
+);
+CREATE Table factura(
+  id_factura INT(11) PRIMARY KEY NOT NULL,
+  id_usuario INT(11) NOT NULL,
+  fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
+);
+CREATE Table ticket(
+  id_ticket INT(11) PRIMARY KEY NOT NULL,
+  nombre VARCHAR(30) NOT NULL,
+  fecha DATE NOT NULL,
+  hora TIME NOT NULL,
+  sala VARCHAR(50) NOT NULL,
+  sitios INT(2) NOT NULL,
+  precio DECIMAL(5, 2) NOT NULL
+);
+CREATE Table detalle(
+  id_detalle INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  id_factura INT(11) NOT NULL,
+  id_ticket INT(11) NOT NULL,
+  fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  cantidad INT(2) NOT NULL,
+  total DECIMAL(5, 2),
+  FOREIGN KEY (id_factura) REFERENCES factura(id_factura),
+  FOREIGN KEY (id_ticket) REFERENCES ticket(id_ticket)
+);
+-- Roles:
 INSERT INTO
   role
 VALUES
   (1, 'jefe'),(2, 'cliente');
-SELECT
-  *
-FROM
-  role;
-CREATE Table usuario(
-    id_usuario INT(11) PRIMARY KEY NOT NULL,
-    id_role INT(11) NOT NULL,
-    email VARCHAR(30) NOT NULL,
-    password VARCHAR(30) NOT NULL,
-    FOREIGN KEY (id_role) REFERENCES role(id_role)
-  );
+-- Usuario:
 INSERT INTO
   usuario
 VALUES
   (1, 1, 'eric@test.com', 'eric123'),
   (2, 2, 'nico@test.com', 'nico123'),
   (3, 2, 'po@test.com', 'po123');
-CREATE Table factura(
-    id_factura INT(11) PRIMARY KEY NOT NULL,
-    id_usuario INT(11) NOT NULL,
-    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
-  );
+-- Factura:
 INSERT INTO
   factura
 VALUES
-  (1, 2, null),(2, 2, null),(3, 3, null),(4, 3, null);
-SELECT
-  *
-FROM
-  factura;
-CREATE Table ticket(
-    id_ticket INT(11) PRIMARY KEY NOT NULL,
-    nombre VARCHAR(30) NOT NULL,
-    fecha DATE NOT NULL,
-    hora TIME NOT NULL,
-    sala VARCHAR(50) NOT NULL,
-    sitios INT(2) NOT NULL,
-    precio DECIMAL(5, 2) NOT NULL
-  );
+  (1, 2, null),(2, 3, null);
+-- Ticket:
 INSERT INTO
   ticket
 VALUES
@@ -76,27 +82,11 @@ VALUES
     5,
     6.50
   );
-SELECT
-  *
-FROM
-  ticket;
-CREATE Table detalle(
-    id_detalle INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    id_factura INT(11) NOT NULL,
-    id_ticket INT(11) NOT NULL,
-    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    cantidad INT(2) NOT NULL,
-    total DECIMAL(5, 2) NOT NULL,
-    FOREIGN KEY (id_factura) REFERENCES factura(id_factura),
-    FOREIGN KEY (id_ticket) REFERENCES ticket(id_ticket)
-  );
+-- Detalle:
 INSERT INTO
   detalle
 VALUES
-  (null, 1, 1, null, 2, 9),
-  (null, 2, 1, null, 2, 9),
-  (null, 3, 2, null, 1, 5.50),
-  (null, 4, 3, null, 1, 6.50);
+  (null, 1, 3, null, 3, 0),(null, 2, 1, null, 2, 0);
 --Consultas:
   --Consultar el usuario, nombre del ticket y el total que pago
 SELECT
@@ -126,4 +116,18 @@ WHERE
 group by
   u.email;
 -- Trigger:
-  -- Hacer un trigger que al hacer una compra de un ticket reste los sitios del tiicket.
+  -- Hacer un trigger que al hacer una compra de un ticket reste los sitios del tiicket y calcule el total en detalle.
+  -- Trigger para retar los sitios:
+  -- DELIMITER $$
+  -- CREATE OR REPLACE TRIGGER sitios_menos AFTER INSERT ON detalle
+  -- FOR EACH ROW
+  -- BEGIN
+  -- UPDATE ticket SET sitios= sitios - NEW.cantidad WHERE id_ticket = NEW.id_ticket;
+  -- END$$
+  -- Trigger para calcular el total (precio*cantidad):
+  -- DELIMITER $$
+  -- CREATE OR REPLACE TRIGGER total_v BEFORE INSERT ON detalle
+  -- FOR EACH ROW
+  -- BEGIN
+  --  SET NEW.total = (SELECT (t.precio*NEW.cantidad) FROM ticket as t WHERE t.id_ticket = NEW.id_ticket);
+  -- END$$
